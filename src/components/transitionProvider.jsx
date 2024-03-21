c09527a24805d2350f151a19143abb4d2c281b22
FrancisNampellah <bnampellah1@gmail.com>
2024-03-22 01:32:53 +0300

    the transition

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -8,7 +8,7 @@ import { motion } from "framer-motion";
 const TransitionProvider = ({ children }) => {
   const pathName = usePathname();
   return (
-    <AnimatePresence>
+    <AnimatePresence mode="wait">
       <div
         key={pathName}
         className="w-screen min-h-full bg-gradient-to-b from-blue-50 to-red-100"
@@ -20,18 +20,20 @@ const TransitionProvider = ({ children }) => {
           transition={{ duration: 0.5, ease: "easeOut" }}
         />
 
-<motion.div
-          className="fixed m-auto  top-0 bottom-0 right-0 text-white text-8xl cursor-default z-50 w-fit h-fit"
-          animate={{ opacity:0 }}
-          initial={{opacity:1}}
-          exit={{ opacity:0 }}
+        <motion.div
+          className="fixed m-auto left-0 top-0 bottom-0 right-0 text-white text-8xl cursor-default z-50 w-fit h-fit"
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 0 }}
+          exit={{ opacity: 0 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
-        >{pathName}</motion.div>
+        >
+          {pathName.substring(1)}
+        </motion.div>
 
         <motion.div
-          className="h-screen w-screen fixed bg-black rounded-t-[100px] bottom-0 z-40"
-          initial={{ height: "140vh" }}
-          animate={{ height: "0vh", transition: { delay: 1 } }}
+          className="h-screen w-screen fixed bg-red-500 rounded-t-[100px] bottom-0 z-30"
+          initial={{ height: "100vh" }}
+          animate={{ height: "0vh", transition: { delay: 0.5 } }}
         />
 
         <div className="h-24">
