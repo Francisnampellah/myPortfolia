0cc009703ca2b1c668abab826d357da1d282dd38
francisnampellah <bnampellah1@gmail.com>
2024-03-28 06:26:35 -0700

    ratio

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -19,28 +19,17 @@ const tabs_data = {
 };
 
 const PortfolioPage = () => {
-  const [pc,setImage ] = useState(tabs_data.tab1);
+  const [pc, setImage] = useState(tabs_data.tab1);
   const [isPhone, setIsPhone] = useState(false);
 
   const size = useWindowSize();
 
-  useEffect(() => 
-  {
-    if(size.width < 768){
-      setImage(tabs_data.tab2)
-      setIsPhone(true)
-    }else(
-      setImage(tabs_data.tab1)
-    )
+  useEffect(() => {
+    if (size.width < 768) {
+      setImage(tabs_data.tab2);
+      setIsPhone(true);
+    } else setImage(tabs_data.tab1);
   }, [size]);
-
-
-
-
-
-
-
-
 
   return (
     <motion.div
@@ -53,7 +42,9 @@ const PortfolioPage = () => {
         <div className="container mt-8 justify-center items-center">
           <Swiper
             navigation
-            pagination={{ type: "fraction" }}
+            pagination={{
+              dynamicBullets: true,
+            }}
             modules={[Navigation, Pagination]}
             onSwiper={(swiper) => console.log(swiper)}
             className="h-[96%] md:h-96 w-[95%] rounded-lg bg-gray-400/30 rounded-lg shadow-md backdrop-blur border border-gray-400/30 flex justify-center items-center"
@@ -61,19 +52,24 @@ const PortfolioPage = () => {
             {pc.map((image, index) => (
               <SwiperSlide key={index}>
                 <div className="flex h-full w-full ">
-                { isPhone ? <Image
-                  src={image}
-                  height={  100     }
-                  width={  100     }
-                    alt={image.alt}
-                  className="flex justify-center items-center h-full w-full object-cover "
-                />:  <Image
-                src={image}
-                fill
-                 alt={image.alt}
-                className="flex justify-center items-center h-full w-full object-contain "
-              />}
+                  {isPhone ? (
+                    <Image
+                      src={image}
+                      height={100}
+                      width={100}
+                      alt={image.alt}
+                      className="flex justify-center items-center h-full w-full object-cover "
+                    />
+                  ) : (
+                    <Image
+                      src={image}
+                      fill
+                      alt={image.alt}
+                      className="flex justify-center items-center h-full w-full object-contain "
+                    />
+                  )}
                 </div>
+                <div>hello</div>
               </SwiperSlide>
             ))}
           </Swiper>
@@ -81,7 +77,7 @@ const PortfolioPage = () => {
       </section>
       <div className="h-fit w-full flex justify-center px-8 pt-4">
         <div className="flex-1 h-fit lg:w-1/2 self-center flex flex-col gap-8 items-center justify-center bg-gray-400/30 rounded-lg shadow-md backdrop-blur border border-gray-400/30 p-4 ">
-           {size.width}
+          {size.width}
         </div>
       </div>
     </motion.div>
